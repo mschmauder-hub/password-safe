05c6c2e19ed8ba3247da98d4e4e54cdde99ac555
mschmauder-hub <marius.schmauder.ms@gmail.com>
2020-08-24 17:12:35 +0200

    Refactor code

--- cli.js
+++ cli.js
@@ -1,31 +1,31 @@
 const inquirer = require("inquirer");
+const { readPassword } = require("./lib/passwords");
+const {
+  askInitialQuestions,
+  askGetPasswordQuestions,
+  askSetPasswordQuestions,
+  CHOICE_GET,
+  CHOICE_SET,
+} = require("./lib/questions");
 const fs = require("fs").promises;
 
-let questions = [
-  {
-    type: "list",
-    name: "password",
-    message: "What do you want to do?",
-    choices: ["Get a password", "Set a password"],
-  },
-  { type: "password", name: "password", message: "Whats your password?" },
-  { type: "input", name: "key", message: "Which password do you need?" },
-];
+async function main() {
+  const { operation } = await askInitialQuestions();
 
-inquirer.prompt(questions).then(async (answers) => {
-  if (answers.password === "123") {
-    try {
-      const passwordsJSON = await fs.readFile("./passwords.json", "utf-8");
-      let passwords = JSON.parse(passwordsJSON);
-      if (passwords[answers.key]) {
-        console.log(passwords[answers.key]);
-      } else {
-        console.log(`You have set no ${answers.key} password`);
-      }
-    } catch (error) {
-      console.log(error);
+  if (operation === CHOICE_GET) {
+    const { masterPassword, key } = await askGetPasswordQuestions();
+
+    if (masterPassword === "123") {
+      const password = await readPassword(key);
+      console.log(password);
+    } else {
+      console.log("password incorrect");
     }
   } else {
-    console.log("password incorrect");
+    const setPassword = await askSetPasswordQuestions();
+    const passwordsJSON = await fs.readFile("./passwords.json", "utf-8");
+    let passwords = JSON.parse(passwordsJSON);
   }
-});
+}
+
+main();
