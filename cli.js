9859e5601e67d7a48f3406555a721ad9997f1279
mschmauder-hub <marius.schmauder.ms@gmail.com>
2020-08-24 13:57:17 +0200

    Add async

--- cli.js
+++ cli.js
@@ -1,14 +1,31 @@
 const inquirer = require("inquirer");
+const fs = require("fs").promises;
 
 let questions = [
+  {
+    type: "list",
+    name: "password",
+    message: "What do you want to do?",
+    choices: ["Get a password", "Set a password"],
+  },
   { type: "password", name: "password", message: "Whats your password?" },
-  { type: "input", name: "name", message: "Whats your name?" },
+  { type: "input", name: "key", message: "Which password do you need?" },
 ];
 
-inquirer.prompt(questions).then((answers) => {
-  console.log(`Hi ${answers.password}`);
+inquirer.prompt(questions).then(async (answers) => {
+  if (answers.password === "123") {
+    try {
+      const passwordsJSON = await fs.readFile("./passwords.json", "utf-8");
+      let passwords = JSON.parse(passwordsJSON);
+      if (passwords[answers.key]) {
+        console.log(passwords[answers.key]);
+      } else {
+        console.log(`You have set no ${answers.key} password`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    console.log("password incorrect");
+  }
 });
-
-// const password = readlineSync.question("What is your password?", {
-//   hideEchoBack: true,
-// });
