aad9fe0956f87e4368ad400d0aa2007530d088a2
mschmauder-hub <marius.schmauder.ms@gmail.com>
2020-08-27 16:23:30 +0200

    Add Post and put function

--- server.js
+++ server.js
@@ -1,24 +1,58 @@
 require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
+app.use(bodyParser.json());
 
 const { MongoClient } = require("mongodb");
+const {
+  readPassword,
+  writePassword,
+  readAllPasswords,
+  updatePassword,
+} = require("./lib/passwords");
+const { decrypt, encrypt } = require("./lib/crypto");
+const { response } = require("express");
 
-app.listen(port, () => {
-  console.log("App is listening ");
-});
+const client = new MongoClient(process.env.MONGO_URI);
 
-app.get("/", (req, res) => {
-  db.collection("passwords")
-    .find()
-    .toArray()
-    .then((results) => {
-      console.log(results);
-    });
+const masterPassword = "123";
 
-  MongoClient.connect(process.env.MONGO_URI).then((client) => {
-    const db = client.db(process.env.MONGO_NAME);
+async function main() {
+  await client.connect();
+  const database = client.db(process.env.MONGO_NAME);
+
+  app.get("/api/passwords/:name", async (req, response) => {
+    const key = req.params.name;
+    const encryptedPassword = await readPassword(key, database);
+    const password = decrypt(encryptedPassword, masterPassword);
+
+    response.send(password);
   });
-});
+  app.get("/passwords", async (req, res) => {
+    const allPasswords = await readAllPasswords(database);
+    res.send(allPasswords);
+  });
+
+  app.post("/create", async (req, response) => {
+    const { key, password } = req.body;
+
+    const encryptedPassword = await encrypt(password, masterPassword);
+
+    await writePassword(key, encryptedPassword, database);
+  });
+
+  app.put("/update", async (req, res) => {
+    console.log("test");
+    await updatePassword(database);
+    res.send("test");
+  });
+
+  app.listen(port, () => {
+    console.log("App is listening ");
+  });
+}
+
+main();
