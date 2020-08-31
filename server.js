f62c9819c5010386ae85191e81307c7b1f9028fc
mschmauder-hub <marius.schmauder.ms@gmail.com>
2020-08-31 16:42:50 +0200

    Add jwt cookie

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
 const createPasswordsRouter = require("./routes/passwords");
 const createUsersRouter = require("./routes/users");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const port = 3000;
 app.use(bodyParser.json());
 app.use(express.json());
+app.use(cookieParser());
 
 const client = new MongoClient(process.env.MONGO_URI, {
   useUnifiedTopology: true,
@@ -21,7 +23,7 @@ async function main() {
   const database = client.db(process.env.MONGO_NAME);
 
   app.use("/api/passwords", createPasswordsRouter(database, masterPassword));
-  app.use("/api/users", createUsersRouter(database));
+  app.use("/api/users", createUsersRouter(database, process.env.JWT_TOKEN));
 
   app.listen(port, () => {
     console.log("App is listening ");
